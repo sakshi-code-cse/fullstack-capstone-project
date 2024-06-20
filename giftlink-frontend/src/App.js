da54b45ee368d27c4ebd565fb723e9a5f95082e4
Isahn Gawande <ishangawande55@gmail.com>
2024-06-20 02:57:13 -0400

    Update backend and frontend App.js files

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,42 +1,30 @@
 import React from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import SearchPage from './components/SearchPage/SearchPage';
-
-
-
 import DetailsPage from './components/DetailsPage/DetailsPage';
 
-
-
-
-
-
 function App() {
-  const navigate = useNavigate();
   return (
-        <>
-        <Navbar/>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/app" element={<MainPage />} />
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/app" element={<MainPage />} />
 
-          <Route path="/app/product/:productId" element={<DetailsPage/>} />
+        <Route path="/app/product/:productId" element={<DetailsPage />} />
 
-          <Route path="/app/login" element={<LoginPage/>} />
-          <Route path="/app/register" element={<RegisterPage />} />
-          <Route path="/app/profile" element={<Profile/>} />
-          <Route path="/app/search" element={<SearchPage/>} />
-
-
-        </Routes>
-        </>
+        <Route path="/app/login" element={<LoginPage />} />
+        <Route path="/app/register" element={<RegisterPage />} />
+        <Route path="/app/profile" element={<Profile />} />
+        <Route path="/app/search" element={<SearchPage />} />
+      </Routes>
+    </>
   );
 }
+
 export default App;
