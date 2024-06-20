aa7f0afec7d3b59d8bfec9f1fc18a6bda152e4fa
ishangawande55 <ishangawande55@gmail.com>
2024-06-20 02:30:19 -0400

    Update App.js and Profile.js with necessary changes

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -6,7 +6,7 @@ import RegisterPage from './components/RegisterPage/RegisterPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-
+import Profile from './components/Profile/Profile';
 import SearchPage from './components/SearchPage/SearchPage';
 
 
@@ -31,7 +31,7 @@ function App() {
 
           <Route path="/app/login" element={<LoginPage/>} />
           <Route path="/app/register" element={<RegisterPage />} />
-
+          <Route path="/app/profile" element={<Profile/>} />
           <Route path="/app/search" element={<SearchPage/>} />
 
 
