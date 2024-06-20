66cd96972529ce4f51a375c1bc33e61b569735ac
ishangawande55 <101445350+ishangawande55@users.noreply.github.com>
2024-06-20 10:56:47 +0530

    Update App.js

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -6,11 +6,10 @@ import RegisterPage from './components/RegisterPage/RegisterPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-<<<<<<< HEAD
+
 import DetailsPage from './components/DetailsPage/DetailsPage';
 
-=======
->>>>>>> origin/main
+
 function App() {
   const navigate = useNavigate();
   return (
@@ -19,12 +18,12 @@ function App() {
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/app" element={<MainPage />} />
-<<<<<<< HEAD
+
           <Route path="/app/product/:productId" element={<DetailsPage/>} />
-=======
+
           <Route path="/app/login" element={<LoginPage/>} />
           <Route path="/app/register" element={<RegisterPage />} />
->>>>>>> origin/main
+
         </Routes>
         </>
   );
